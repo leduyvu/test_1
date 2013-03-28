c96feac24b6f1c3900a31691f6f4354c9b6d5763
leduyvu <le.duy.vu@framgia.com>
2013-03-28 09:57:28 +0700

    hello app

---
+++ test_1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = '503318cf11ec4addd8320cbd119d8872c3b6280acddbfeae1a334adc450070afec7af54e8caa30ef9cffdf53e9cbea45044a1d8984b18d9f0fff9155881432f7'
